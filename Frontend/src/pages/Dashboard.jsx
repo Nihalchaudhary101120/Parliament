4a27b8b8feb8cc9df2b7f04f5d674c8ce2a98198
Nihal <chaudharynihal299@gmail.com>
2026-01-27 00:08:42 +0530

    kuch banaya

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,46 @@
-import react from 'react';
+import React from 'react';
 import "./Dashboard.css";
+import accountimg from"../assets/account.png";
 
 const DashBoard = () => {
     return (
 
         <>
 
-            <div class="glass-panel">
-                <h2 class="panel-title ">GAME MODE</h2>
+            <h1 className="logo-name">PARLIAMENT  BATTLEGROUND</h1>
+            <h2 className="quote">Control The Flow Of Nation</h2>
 
-                <button class="glass-btn sharp-btn">🎮 Player VS Computer</button>
-                <button class="glass-btn sharp-btn">🌐 Online Multiplayer</button>
-                <button class="glass-btn sharp-btn">👥 Play with Friends</button>
+            <div>
+                 <img src={accountimg} className="profile"></img>
             </div>
 
+            <div className="glass-panel">
+                <h2 className="panel-title ">GAME MODE</h2>
+
+                <button className="glass-btn sharp-btn">🎮 Player VS Computer</button>
+                <button className="glass-btn sharp-btn">🌐 Online Multiplayer</button>
+                <button className="glass-btn sharp-btn">👥 Play with Friends</button>
+            </div>
+
+            <div className="bottom-bar">
+                <button className="nav-btn">⚙️<span>Settings</span></button>
+                <button className="nav-btn">📩<span>Inbox</span></button>
+                <button className="nav-btn">👥<span>Friends</span></button>
+            </div>
+
+
+            <div className="glass-panel2">
+                <h2 className="panel-title">Lobby</h2>
+                <hr></hr>
+                <div className="friend" >
+                
+                     <p> <button>+</button> Nihal <span>online</span>      </p>
+                     <p> <button>+</button> Shlok  <span>online</span>     </p>
+                     <p> <button>+</button> Tanmay  <span>online</span>    </p>
+                     <p> <button>+</button> Dhangar <span>offline</span>    </p>
+                </div>                
+
+            </div>
 
         </>
 
