4f3372a736d307328fc6d794ff0fddbe63b9c36f
Tanmaysingh2210 <tanmayrajpoot2210@gmail.com>
2026-03-29 14:29:44 +0530

    session fixing

--- Server/app.js
+++ Server/app.js
@@ -10,6 +10,7 @@ import MongoStore from "connect-mongo";
 
 connectDB();
 const app = express();
+app.set('trust proxy', 1);
 app.use(express.json());
 
 const allowedOrigins = [
@@ -28,10 +29,11 @@ app.use(cors({
     },
     credentials: true
 }));
+
 const sessionMiddleWare = session({
     secret: process.env.Secret,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     store: MongoStore.create({
         mongoUrl: process.env.ATLAS_URI,
         collectionName: "sessions"
@@ -39,7 +41,8 @@ const sessionMiddleWare = session({
     cookie: {
         secure: true,
         httpOnly: true,
-        sameSite: "none"
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24 * 7 
     }
 });
 app.use(sessionMiddleWare);
