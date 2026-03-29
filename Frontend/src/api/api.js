f117189e47a896252136eab98821663a1fef3b25
nihal chaudhary <chaudharynihal299@gmail.com>
2026-03-29 12:56:14 +0530

    Change API base URL to production server

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://localhost:3000',
+    baseURL:"https://parliamentbackend.onrender.com",
     withCredentials:true, // send/ receive cookies
     headers:{
         'Content-Type' : 'application/json'
