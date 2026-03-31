5fc7a6725f73e67ac65ac153923f6a717f783300
shlok <shlokbhatia16@gmail.com>
2026-04-01 02:03:57 +0530

    guest

--- Frontend/src/pages/EntryPage.jsx
+++ Frontend/src/pages/EntryPage.jsx
@@ -1,20 +1,41 @@
 import "./EntryPage.css";
 import { useAuth } from "../context/AuthContext.jsx";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const [loading, setLoading] = useState(false);
 
 export default function EntryPage() {
+
+
     const { handleGuest } = useAuth();
     const navigate = useNavigate();
 
 
     return (
         <>
+
+            {loading && (
+                <div className="entry-loading">
+                    <div className="entry-spinner"></div>
+                    <p>Entering Battlefield...</p>
+                </div>
+            )}
             <div className="entry-container">
                 <div className="entry-card">
                     <h1 className="entry-title">PARLIAMENT BATTLEGROUND</h1>
                     <p className="entry-subtitle">Choose how you want to enter</p>
 
-                    <button className="entry-btn guest" onClick={handleGuest}>Continue as Guest</button>
+                    <button className="entry-btn guest"
+                        disabled={loading}
+                        onClick={async () => {
+                            setLoading(true);
+
+                            // 🔥 allow UI to update
+                            await new Promise(res => setTimeout(res, 50));
+
+                            await handleGuest();
+                        }}>{loading ? "Entering..." : "Continue as Guest"}</button>
                     <button className="entry-btn login" onClick={() => navigate("/login")}>
                         Login
                     </button>
