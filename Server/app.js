84ec1bde5c70a235148588135abbb6cfa6bc61e8
shlok <shlokbhatia16@gmail.com>
2026-04-08 20:02:40 +0530

    s

--- Server/app.js
+++ Server/app.js
@@ -42,7 +42,7 @@ const sessionMiddleWare = session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'production'? 'none' : 'lax',
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
 });
